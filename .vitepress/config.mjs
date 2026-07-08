6b1b4906ed0a90717536eecb84e44e6a68ed0318
MM125JD <dkksoaos@gmail.com>
2026-07-08 16:46:28 +0800

    fix: set base path for github pages

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "chenduiblog",
   description: "A VitePress Site",
+  base: '/new/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
