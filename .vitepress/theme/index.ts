3f97af0d57c60459d99c4ecc0175cfa55bed5da1
MM125JD <dkksoaos@gmail.com>
2026-07-14 09:28:49 +0800

    pink

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,7 +4,7 @@ import DefaultTheme from "vitepress/theme";
 import type { Theme } from 'vitepress'
 
 // 1. 引入官方猫咪皮肤（深色摩卡风味 + 薰衣草紫强调色）
-import "@catppuccin/vitepress/theme/mocha/lavender.css"; 
+import "@catppuccin/vitepress/theme/mocha/pink.css";
 
 // 2. 🌟 引入你自己的自定义样式文件！名字对上就完全没问题
 import './custom.css'; 
