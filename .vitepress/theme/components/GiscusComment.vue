edb561b50bd00f889ee3ad29f6e77b99c6dd6589
MM125JD <dkksoaos@gmail.com>
2026-07-14 11:47:41 +0800

    增加评论

---
+++ .vitepress/theme/components/GiscusComment.vue
@@ -0,0 +1,32 @@
+<!-- .vitepress/theme/components/GiscusComment.vue -->
+<template>
+  <div class="giscus-comment-container">
+    <!-- Giscus 评论组件渲染的宿主元素 -->
+    <component 
+      :is="'script'"
+      src="https://giscus.app/client.js"
+      data-repo="MM125JD/new"
+      data-repo-id="R_kgDOTRXwDw"
+      data-category="General"
+      data-category-id="DIC_kwDOTRXwD84DBJb8"
+      data-mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="bottom"
+      data-theme="preferred_color_scheme"
+      data-lang="zh-CN"
+      crossorigin="anonymous"
+      async
+    />
+  </div>
+</template>
+
+<style scoped>
+/* 给评论区加一点顶部的间距，让它和你的正文内容隔开，排版更美观 */
+.giscus-comment-container {
+  margin-top: 40px;
+  border-top: 1px dashed var(--vp-c-divider);
+  padding-top: 24px;
+}
+</style>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,6 +2,9 @@
 // .vitepress/theme/index.ts
 import DefaultTheme from "vitepress/theme";
 import type { Theme } from 'vitepress'
+import { h } from 'vue'
+// @ts-ignore
+import GiscusComment from './components/GiscusComment.vue'
 
 // 1. 引入官方猫咪皮肤（深色摩卡风味 + 薰衣草紫强调色）
 import "@catppuccin/vitepress/theme/mocha/pink.css";
@@ -11,4 +14,11 @@ import './custom.css';
 
 export default {
   extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      // doc-after 是 VitePress 官方提供的插槽，代表在文档正文的下方注入内容
+      'doc-after': () => h(GiscusComment)
+    })
+  }
+
 } satisfies Theme
